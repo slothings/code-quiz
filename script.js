c93dd0ebd79cea43696dfb2d28361ecb87bc72d7
Slothing <seth.r.martineau@gmail.com>
2020-08-14 16:03:21 -0600

    fixed some styling for my index.html and started making my questions in script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+// Making questions and got my questions from https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "<scripting>",
+            b: "<script>",
+            c: "<javascript>",
+            d: "<js>"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "Which built-in method returns the length of the string?",
+        answers: {
+            a: "length()",
+            b: "size()",
+            c: "index()",
+            d: "None of the above"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "Which of the following type of variable is visible everywhere in your Javascript Code?",
+        answers: {
+            a: "global variable",
+            b: "local variable",
+            c: "Both of the above",
+            d: "None of the above"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+        answers: {
+            a: "toSource()",
+            b: "valueOf()",
+            c: "toString()",
+            d: "None of the above"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "Which of the following function of Array object joins all elements of an array into a string?",
+        answers: {
+            a: "concat()",
+            b: "join()",
+            c: "pop()",
+            d: "map()"
+        },
+        correctAnswer: "b"
+    }
+];
+
+// Declaring variables
+
+var score = 0;
+
+var questionIndex = 0;
+
+var timer = document.getElementById("startTime");
+
+var currentTime = document.getElementById("currentTime");
+
+
+
